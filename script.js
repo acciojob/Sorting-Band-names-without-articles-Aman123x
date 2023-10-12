00ed2e9cdb44c857d713d7489b9e1df45b588ff4
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-10-12 21:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
+function sortBandNames(names) {
+  return names.sort((a, b) => {
+    // Function to remove articles and leading spaces
+    const removeArticles = (name) => name.replace(/^(a|an|the) /i, '');
 
+    const nameA = removeArticles(a).toLowerCase();
+    const nameB = removeArticles(b).toLowerCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+// Sort the band names
+sortBandNames(bandNames);
+
+// Get the ul element by its ID
+const ulElement = document.getElementById('band');
+
+// Iterate over the sorted band names and create li elements
+bandNames.forEach((bandName) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = bandName;
+  ulElement.appendChild(liElement);
+});
